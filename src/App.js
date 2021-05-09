7a6d933a3cf7548c41302621112e46fcf141d16f
James <jamesmca@live.com>
2021-05-09 12:40:54 -0700

    initialized basic components and access to local storage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
+import './styles.css'
+import React, { useState, useEffect } from 'react'
+
 
 function App() {
+  const [nav, setNav] = useState(0)
+  const [days, setDays] = useState([])
+  const [dateDisplay, setDateDisplay] = useState('')
+  const [clicked, setClicked] = useState()
+  const [events, setEvents] = useState(
+    localStorage.getItem('events') ? 
+    JSON.parse(localStorage.getItem('events')) : 
+    [])
+
+  function eventForDate(date) {
+    events.find(e => e.date === date)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('events', JSON.stringify(events))
+  }, [events])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ src/components/Day.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Day = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Day
